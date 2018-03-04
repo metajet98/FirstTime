dfdb126b1ac4126d12022227d57f9bbfafbdc640
metajet98 <bac.nguyenxuan98@gmail.com>
2018-03-04 23:48:40 +0700

    Sửa lần 1

--- TestGithub/Form1.cs
+++ TestGithub/Form1.cs
@@ -24,7 +24,7 @@ namespace TestGithub
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World!");
+            MessageBox.Show("Chao the gioi!");
         }
     }
 }
